f1f56556f41fe612fa6757280ec0e3c2055b63f0
Your Name <you@example.com>
2023-07-20 23:15:00 +0100

    module pattern

--- script.js
+++ script.js
@@ -5,13 +5,14 @@
 // import { add } from './clean';
 // import { bigExpenses } from './clean';
 console.log('Importing module');
-import * as ShoppigCart from './shoppingCart.js';
+// import * as ShoppigCart2 from './shoppingCart.js';
 
 // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 // const data = await res.json();
 // console.log(data);
 
 import add, { cart } from './shoppingCart.js';
+/*
 add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
@@ -33,3 +34,27 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return { addToCart, cart, totalPrice, totalQuantity };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+// ShoppingCart2.orderStock('mosleu', 5);
+// ShoppingCart2.orderStock('avocado', 3);
